93629e6341410de2a908aa54eb4db8e0d5229b27
Irene Saez <bluemilk44@gmail.com>
2019-01-25 00:08:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(call)# code an argument here
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  planeteer_calls.map |element|
+  planeteer_calls.collect do |element|
   element.capitalize << !
 end
 end
