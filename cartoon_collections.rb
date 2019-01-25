0bde458f0d55170b1f8282c20aec0751d9add052
Irene Saez <bluemilk44@gmail.com>
2019-01-25 00:00:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index do |name, number|
-  puts "#{number}"
+  puts "#{number + 1},"
 end
 
 def summon_captain_planet# code an argument here
