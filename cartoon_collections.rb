c077c76968d42ecf13c19ec1c2a94d290df9426f
Irene Saez <bluemilk44@gmail.com>
2019-01-25 00:13:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,7 @@ def summon_captain_planet(veggies)# code an argument here
   veggies.collect do |name|
   name.capitalize << "!"
 end
+end
 
 def long_planeteer_calls# code an argument here
   # Your code here
